07fa1b6c6c140c5ca854f845560e6d1c9f67b5ff
Trent Deckard <tdeckard2000@yahoo.com>
2023-03-20 19:54:31 -0400

    prevent iOS input zoom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -112,7 +112,7 @@ export default function Home() {
 			<Head>
 				<title>Shipwrecks.pro</title>
 				<meta name="description" content="Interactive Shipwreck Map" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>
