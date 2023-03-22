dd765d1865e1240bbd0dd52aea4a6e5a807d8f73
Trent Deckard <tdeckard2000@yahoo.com>
2023-03-22 16:44:56 -0400

    fix race condition causing intermittent error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,15 +30,9 @@ export default function Home() {
 	})
 
 	useEffect(() => {
-		if (map.current) return;
-		map.current = new mapboxgl.Map({
-			container: mapContainer.current,
-			style: "mapbox://styles/mapbox/streets-v12",
-			center: [lng, lat],
-			zoom: zoom,
-		});
+		console.log("init page")
 		initializePage();
-	});
+	}, []);
 
 	const initializePage = async () => {;
 		const newShipList = await clientAPI.getAllShipwrecks();
@@ -46,30 +40,35 @@ export default function Home() {
 		initializeMap(newShipList);
 	}
 
-	const initializeMap = (shipList: Shipwreck[]) => {
-		if(!map.current || map.current === null) return;
+	const initializeMap = async (shipList: Shipwreck[]) => {
+		if (map.current) return;
+		map.current = await new mapboxgl.Map({
+			container: mapContainer.current,
+			style: "mapbox://styles/mapbox/streets-v12",
+			center: [lng, lat],
+			zoom: zoom,
+		});
 		//@ts-ignore
-		map.current.loadImage('map-prettypurple-icon.png', (error, image) => {map.current.addImage('iconPurple', image)});
-		//@ts-ignore
-		map.current.loadImage('map-green-icon.png', (error, image) => {map.current.addImage('iconGreen', image)});
-		updateMapMarkers(shipList);
-		initializeMarkerPopup();
+		map.current.on('load', () => {
+			console.log("MAP LOAD")
+			//@ts-ignore
+			map.current.loadImage('map-prettypurple-icon.png', (error, image) => {map.current.addImage('iconPurple', image)});
+			//@ts-ignore
+			map.current.loadImage('map-green-icon.png', (error, image) => {map.current.addImage('iconGreen', image)});
+			updateMapMarkers(shipList);
+			defineMarkerInteractions();
+		})
 	}
 
-	const initializeMarkerPopup = () => {
+	const defineMarkerInteractions = () => {
 		//@ts-ignore
 		map.current.on('mouseenter', 'places', (e) => {
-			popup.remove();
-			//@ts-ignore
-			map.current.getCanvas().style.cursor = 'pointer';
-			console.log(e.features)
-			const coordinates = e.features[0].geometry.coordinates.slice();
-			const description = e.features[0].properties.description;
-			while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-				coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-			}
-			//@ts-ignore
-			popup.setLngLat(coordinates).setHTML(description).addTo(map.current);
+			e.features[0].layer.layout['icon-image'] = 'iconGreen'
+			handleMarkerHover(e);
+		});
+		//@ts-ignore
+		map.current.on('click', 'places', (e) => {
+			handleMarkerClick(e);
 		})
 		//@ts-ignore
 		map.current.on('mouseleave', 'places', () => {
@@ -79,13 +78,36 @@ export default function Home() {
 		});
 	}
 
+	const handleMarkerClick = (marker: any) => {
+		console.log("marker clicked")
+		popup.remove();
+		marker.features[0].layer.layout['icon-image'] = 'iconGreen'
+		console.log("moddedMarker: ", marker)
+	}
+
+	const handleMarkerHover = (marker: any) => {
+		//@ts-ignore
+		map.current.getCanvas().style.cursor = 'pointer';
+		console.log(marker)
+		const coordinates = marker.features[0].geometry.coordinates.slice();
+		const description = marker.features[0].properties.description;
+		while (Math.abs(marker.lngLat.lng - coordinates[0]) > 180) {
+			coordinates[0] += marker.lngLat.lng > coordinates[0] ? 360 : -360;
+		}
+		//@ts-ignore
+		popup.setLngLat(coordinates).setHTML(description).addTo(map.current);
+	}
+
 	const handleFilterChange = async (filterParams: ShipwreckFilters) => {
+		if(!map.current) return
+		console.log("filter change triggered")
 		const newShipList = await clientAPI.getFilteredShipwrecks(filterParams);
 		setShipList(newShipList);
 		updateMapMarkers(newShipList);
 	}
 
 	const resetFilters = async () => {
+		if(!map.current) return
 		setFiltersActive(false);
 		const newShipList = await clientAPI.getAllShipwrecks();
 		setShipList(newShipList);
@@ -93,20 +115,21 @@ export default function Home() {
 	}
 
 	const updateMapMarkers = (listOfShips: Shipwreck[]) => {
+		if(!map.current || map.current === null) return
 		const features = [];
+		let index = 0;
 		for(let shipwreck of listOfShips) {
-			console.log(shipwreck._id)
 			features.push({
-				id: shipwreck._id,
+				id: index,
 				type: 'Feature',
 				properties: {
 					description: 
 					`<div style="font-size: 14px; font-weight: 600; margin: 0 0 5px 0;">${shipwreck.name}</div>
 					<div style="width: 200px">
-						<img style="border-radius: 6px;	width: 200px;" src="${shipwreck.linkImage !== undefined ? shipwreck.linkImage : ''}" alt="" />
+						<img style="border-radius: 6px;	max-height: 200px; width: 200px;" src="${shipwreck.linkImage !== undefined ? shipwreck.linkImage : ''}" alt="" />
 					</div>
 					<div>
-						<div><span style="font-weight: 600">Sank:</span> ${shipwreck.dateSunk?.toString().slice(4, 6)}/${shipwreck.dateSunk?.toString().slice(6, 8)}/${shipwreck.dateSunk?.toString().slice(0, 4)}</div>
+						<div><span style="font-weight: 600">Sank:</span> ${ shipwreck.dateSunk != 0 ? shipwreck.dateSunk?.toString().slice(4, 6) + '/' + shipwreck.dateSunk?.toString().slice(6, 8) + '/' + shipwreck.dateSunk?.toString().slice(0, 4) : '?'} </div>
 						<div><span style="font-weight: 600">Shallowest Depth:</span> ${shipwreck.stats.waterDepth !== undefined ? shipwreck.stats.waterDepth : '?'} ft</div>
 						<div><span style="font-weight: 600">Ship Length:</span> ${shipwreck.stats.length !== undefined ? shipwreck.stats.length : '?'} ft</div>
 					</div>
@@ -117,6 +140,7 @@ export default function Home() {
 					coordinates: [shipwreck.coordinates.longitude || 0, shipwreck.coordinates.latitude || 0]
 				}
 			})
+			index++;
 		}
 		//@ts-ignore
 		if(map.current.getLayer('places')) {
@@ -126,7 +150,7 @@ export default function Home() {
 		//@ts-ignore
 		if(map.current.getSource('places')) {
 			//@ts-ignore
-			map.current.removeSource('places')
+			map.current.removeSource('places');
 		}
 		//@ts-ignore
 		map.current.addSource('places', {
